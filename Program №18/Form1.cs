43c3444a322ef093ebe39e6f239b7680971a3568
BlagodarovDemid <d.a.Blagodarov@gmail.com>
2023-03-26 14:47:56 +0300

    Вывод массива в ListBox строчным методом

--- Program №18/Form1.cs
+++ Program №18/Form1.cs
@@ -5,9 +5,11 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
 
 namespace Program__18
 {
@@ -16,13 +18,19 @@ namespace Program__18
         public Form1()
         {
             InitializeComponent();
+            label6.Text = ("00:00:00:000");
         }
         DateTime date1 = new DateTime(0,0);
-        static int[] BubbleSort(int[] mas)
+        int[] mas = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int[] result;
+        int count = 0;
+        bool flag = false;
+        static int[] BubbleSort(int[] mas, DateTime date1, bool flag)
         {
             int temp;
             for (int i = 0; i < mas.Length; i++)
             {
+                date1 = date1.AddMilliseconds(1);
                 for (int j = i + 1; j < mas.Length; j++)
                 {
                     if (mas[i] > mas[j])
@@ -33,6 +41,7 @@ namespace Program__18
                     }
                 }
             }
+            flag = true;
             return mas;
         }
         public static void Swap(int array, int array1)
@@ -73,21 +82,57 @@ namespace Program__18
         {
 
         }
+        public void AddItemsListBox(int first = -1, int second = -1)
+        {
+            listBox1.Items.Add("");
+            foreach (var item in mas)
+            {
+                if (item == first || item == second)
+                {
+                    listBox1.Items[count] += '[' + Convert.ToString(item) + ']' + " ";
+                }
+                else
+                {
+                    listBox1.Items[count] += Convert.ToString(item) + " ";
+                }
+            }
+            count++;
+        }
         private void button1_Click(object sender, EventArgs e)
         {
             if (radioButton1.Checked)
             {
-                date1 = date1.AddMilliseconds(1);
-                label6.Text = date1.ToString("hh:mm:ss:fff");
+                result = BubbleSort(mas, date1, flag);
+                this.AddItemsListBox();
+                listBox1.Text += Convert.ToString(result) + "\n";
+                //label6.Text = date1.ToString("hh:mm:ss:fff");
             }
         }
-
+        /*            if (Context.array != null)
+                    {
+                        if (radioButton1.Checked == true)
+                        {
+                            this.context = new Context(new BubbleSort());
+                            context.ExecuteAlgorithm();
+                            this.AddItemsListBox();
+                            IOFile.SaveData();
+                            buttonSort.Enabled = false;
+                        }
+                        if (radioButton2.Checked == true)
+                        {
+                            this.context = new Context(new ShellSort());
+                            context.ExecuteAlgorithm();
+                            this.AddItemsListBox();
+                            IOFile.SaveData();
+                            buttonSort.Enabled = false;
+                        }
+                            IOFile.content = "";
+                    }
+                    else
+                    {
+                        MessageBox.Show("Массив пуст, сортировка невозможна");
+                    }*/
         private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
         {
 
         }
